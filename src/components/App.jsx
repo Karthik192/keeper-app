1cc9c35dcbefac4d3b1b009f417ebb4a84a3a94e
Karthik Kirito <devakarthik03102001ece@gmail.com>
2022-01-15 11:34:41 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Note from "./Note";
+import Header from "./Header";
+import Footer from "./Footer";
+
+function App() {
+    return <div>
+        <Header />
+        <Note />
+        <Footer />
+    </div>;
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+var currentYear = new Date().getFullYear();
+
+function Footer() {
+    return <footer>
+        <p>©Copyright {currentYear}</p>
+    </footer>;
+}
+
+export default Footer;
