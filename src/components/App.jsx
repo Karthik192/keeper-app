7b5e3f38d38e4a18cf39a52dba34209b25246d36
Karthik Kirito <devakarthik03102001ece@gmail.com>
2022-01-16 12:13:30 +0530

    Keeper v2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import Note from "./Note";
 import Header from "./Header";
 import Footer from "./Footer";
+import notes from '../notes';
 
 function App() {
     return <div>
         <Header />
-        <Note />
+        {notes.map(note =>
+            <Note
+                key={note.key}
+                title={note.title}
+                content={note.content}
+            />
+        )}
         <Footer />
     </div>;
 }
